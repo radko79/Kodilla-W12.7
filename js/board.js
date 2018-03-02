08f17db2f1556f78f807c2f92830a24f2269c5d1
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-02 12:32:52 +0000

    first commit

---
+++ js/board.js
@@ -0,0 +1,37 @@
+var board = {
+	name: 'Tablica Kanban',
+	createColumn: function(column) {
+	  this.element.append(column.element);
+	  initSortable();
+	},
+	element: $('#board .column-container')
+};
+
+$('.create-column')
+	.click(function() {
+		var columnName = prompt('Enter a column name');
+		$.ajax({
+			url: baseUrl + '/column',
+			method: 'POST',
+			data: {
+				name: columnName
+			},
+			success: function(response) {
+				var column = new Column(response.id, columnName);
+				board.createColumn(column);
+			}
+		});
+	});
+
+
+// $('.create-column')
+// 	.click(function(){
+// 		board.createColumn(new Column(prompt('Wpisz nazwe kolumny')));
+// 	});
+	
+function initSortable() {
+    $('.card-list').sortable({
+      connectWith: '.card-list',
+      placeholder: 'card-placeholder'
+    }).disableSelection();
+  }
